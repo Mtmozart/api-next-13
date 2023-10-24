867e3dbed380992ae3d5156cc35940411e8ce454
Mtmozart <mmsnborges@gmail.com>
2023-10-23 21:29:31 -0300

    first commit

--- src/components/pokedex/index.tsx
+++ src/components/pokedex/index.tsx
@@ -1,7 +1,12 @@
+"sever side";
 import Image from "next/image";
 import PokedexImage from "../../../public/images/pokedex.png";
 import styles from "./styles.module.scss";
-export default function Pokedex() {
+
+export default async function Pokedex() {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/1`);
+  const data = await response.json();
+
   return (
     <section className={styles.container}>
       <Image src={PokedexImage} alt="pokedex-image" />
